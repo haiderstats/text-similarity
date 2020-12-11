01eb6c97ae5ad1a231f56c30c49949db212c85f1
Humza <humza@haiderstats.com>
2020-12-11 11:56:59 -0600

    specify host

--- text_similarity/__main__.py
+++ text_similarity/__main__.py
@@ -2,7 +2,7 @@ import uvicorn  # type: ignore
 
 
 def main():
-    uvicorn.run("text_similarity.main:app", port=8000, log_level="info")
+    uvicorn.run("text_similarity.main:app", host="0.0.0.0", port=8000, log_level="info")
 
 
 if __name__ == "__main__":
