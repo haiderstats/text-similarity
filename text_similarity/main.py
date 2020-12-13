8c71100e43f8137c48361b43f05ba464ca88c386
Humza <humza@haiderstats.com>
2020-12-13 10:19:03 -0600

    make route similarity

--- text_similarity/main.py
+++ text_similarity/main.py
@@ -7,11 +7,11 @@ from text_similarity.similarity import Texts
 app = FastAPI()
 
 
-@app.post("/")
+@app.post("/similarity")
 def text_similarity(
     texts: Texts,
     ngram_limit: int = Query(
-        3, description="The highest ngram used for comparision.", gt=0, le=5
+        3, description="The highest ngram used for comparision.", ge=1, le=5
     ),
 ) -> Dict[str, float]:
     return {"similarity": texts.similarity(ngram_limit)}
